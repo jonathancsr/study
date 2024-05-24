7376a172f67c975f1d0b67c43ce813d0c8dda43f
jonathancsr <jonathancsrr@gmail.com>
2024-05-24 16:51:22 -0300

    feat: add router

--- react/ignite-timer/src/App.tsx
+++ react/ignite-timer/src/App.tsx
@@ -1,17 +1,16 @@
-import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
-import { GlobalStyle } from './styles/global'
-import { defaultTheme } from './styles/themes/default'
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { Router } from "./Router";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="success" />
-      <Button />
     </ThemeProvider>
-  )
+  );
 }
--- react/ignite-timer/src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer, ButtonVariants } from './Button.style'
-
-type ButtonProps = {
-  variant?: ButtonVariants
-}
-
-export const Button = ({ variant = 'green' }: ButtonProps) => {
-  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
-}
