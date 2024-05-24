d730044412a58311bf2cea1f5f6e899d33677b71
jonathancsr <jonathancsrr@gmail.com>
2024-05-24 17:33:48 -0300

    feat: layout e header

---
+++ react/ignite-timer/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import { Scroll, Timer } from "phosphor-react";
+import { NavLink } from "react-router-dom";
+import logoIgnite from "../../assets/logo-ignite.svg";
+import { HeaderContainer } from "./style";
+export default function Header() {
+  return (
+    <HeaderContainer>
+      <img src={logoIgnite} alt="" />
+      <nav>
+        <NavLink to="/" title="timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="history">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  );
+}
