6504f3afd12b29628e549242f463ac71a22710fa
jonathancsr <jonathancsrr@gmail.com>
2024-05-26 12:28:15 -0300

    feat: create new cycle

--- react/ignite-timer/src/pages/Home/index.tsx
+++ react/ignite-timer/src/pages/Home/index.tsx
@@ -3,6 +3,7 @@ import { Play } from "phosphor-react";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 
+import { useState } from "react";
 import {
   CountdownContainer,
   FormContainer,
@@ -20,7 +21,17 @@ const newCycleFormValidationSchema = z.object({
 
 type NewCycleFormData = z.infer<typeof newCycleFormValidationSchema>;
 
+type Cycle = {
+  id: string;
+  task: string;
+  minutesAmount: number;
+};
+
 export default function Home() {
+  const [cycles, setCycles] = useState<Cycle[]>([]);
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState<number>(0);
+
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
@@ -29,13 +40,31 @@ export default function Home() {
     },
   });
 
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    const newCycle: Cycle = {
+      id: String(new Date().getTime()),
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+    };
+
+    setCycles((state) => [...state, newCycle]);
+    setActiveCycleId(newCycle.id);
+
+    reset();
+  }
+
   const task = watch("task");
   const isSubmitDisabled = !task;
 
-  function handleCreateNewCycle(data: NewCycleFormData) {
-    console.log(data);
-    reset();
-  }
+  const activeCycle = cycles.find((c) => c.id === activeCycleId);
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
+
+  const minutesAmount = Math.floor(currentSeconds / 60);
+  const secondsAmount = currentSeconds % 60;
+
+  const minutes = String(minutesAmount).padStart(2, "0");
+  const seconds = String(secondsAmount).padStart(2, "0");
 
   return (
     <HomeContainer>
@@ -62,11 +91,11 @@ export default function Home() {
         </FormContainer>
 
         <CountdownContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
           <Separator>:</Separator>
-          <span>0</span>
-          <span>0</span>
+          <span>{seconds[0]}</span>
+          <span>{seconds[1]}</span>
         </CountdownContainer>
         <StartCountdownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} /> Start
