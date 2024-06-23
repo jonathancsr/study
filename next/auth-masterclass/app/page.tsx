9f5ba37011b77bc758f1cec7e6285b431f996df1
jonathancsr <jonathancsrr@gmail.com>
2024-06-23 18:38:14 -0300

    feat: initial commit auth masterclass

---
+++ next/auth-masterclass/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+   <h1 className="font-semibold text-blue-300">
+    Hello Auth
+    <Button variant="custom">Ola </Button>
+   </h1>
+  );
+}
